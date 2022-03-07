67cb4c795e30d4a4d254b5122c8ab7e302f517c8
dragonmux <git@dragonmux.network>
2022-03-07 04:21:33 -0500

    gateware: usb.control.request: Implemented handling for the feature unit's mute and volume controls

--- gateware/audioInterface/usb/control/request.py
+++ gateware/audioInterface/usb/control/request.py
@@ -1,6 +1,8 @@
-from amaranth import Module, Signal, Cat
+from amaranth import Module, Signal, Array, Cat
 from usb_protocol.types import USBRequestType, USBRequestRecipient, USBStandardRequests
-from usb_protocol.types.descriptors.uac3 import AudioClassSpecificRequestCodes, AudioControlInterfaceControlSelectors
+from usb_protocol.types.descriptors.uac3 import (
+	AudioClassSpecificRequestCodes, AudioControlInterfaceControlSelectors, FeatureUnitControlSelectors
+)
 from luna.gateware.usb.usb2.request import (
 	USBRequestHandler, SetupPacket, StallOnlyRequestHandler, USBInStreamInterface, USBOutStreamInterface
 )
@@ -21,8 +23,18 @@ class AudioRequestHandler(USBRequestHandler):
 		super().__init__()
 		# Used to support getting/setting the power state of the device.
 		self.powerState = Signal(8)
+		# Mutes for the various channels
+		self.muteStates = Array(Signal(1, name = f'mute{i}') for i in range(3))
+		# Volume levels for the various channels
+		self.volumeStates = Array(Signal(16, name = f'volume{i}') for i in range(3))
+		# Alt-mode settings to propegate to the rest of the gateware
 		self.interfaces = interfaces
 		self.altModes = {interface: Signal(8, name = f'altMode{interface}') for interface in self.interfaces}
+
+		# Internals
+		self._powerSelect = Signal()
+		self._muteSelect = Signal()
+		self._volumeSelect = Signal()
 
 	def elaborate(self, platform):
 		m = Module()
@@ -31,10 +43,27 @@ class AudioRequestHandler(USBRequestHandler):
 
 		rxTriggered = Signal()
 
-		m.submodules.layout1Transmitter = layout1Transmitter = \
-			StreamSerializer(data_length = 2, domain = 'usb', stream_type = USBInStreamInterface, max_length_width = 2)
-		m.submodules.layout1Receiver = layout1Receiver = \
-			StreamDeserializer(dataLength = 2, domain = 'usb', streamType = USBOutStreamInterface, maxLengthWidth = 2)
+		m.submodules.transmitter = transmitter = StreamSerializer(
+			data_length = 8, domain = 'usb', stream_type = USBInStreamInterface, max_length_width = 4
+		)
+		m.submodules.receiver = receiver = StreamDeserializer(
+			dataLength = 8, domain = 'usb', streamType = USBOutStreamInterface, maxLengthWidth = 4
+		)
+
+		m.d.usb += [
+			self._powerSelect.eq(
+				(setup.index[0:8] == 0) & (setup.index[8:16] == 10) & (setup.value[0:8] == 0) &
+				(setup.value[8:16] == AudioControlInterfaceControlSelectors.AC_POWER_DOMAIN_CONTROL)
+			),
+			self._muteSelect.eq(
+				(setup.index[0:8] == 0) & (setup.index[8:16] == 2) & (setup.value[0:8] >= 0) &
+				(setup.value[0:8] <= 2) & (setup.value[8:16] == FeatureUnitControlSelectors.FU_MUTE_CONTROL)
+			),
+			self._volumeSelect.eq(
+				(setup.index[0:8] == 0) & (setup.index[8:16] == 2) & (setup.value[0:8] >= 0) &
+				(setup.value[0:8] <= 2) & (setup.value[8:16] == FeatureUnitControlSelectors.FU_VOLUME_CONTROL)
+			)
+		]
 
 		with m.If(self.handlerCondition(setup)):
 			with m.FSM(domain = 'usb'):
@@ -67,17 +96,17 @@ class AudioRequestHandler(USBRequestHandler):
 				with m.State('GET_INTERFACE'):
 					# Hook up the transmitter ...
 					m.d.comb += [
-						layout1Transmitter.stream.connect(interface.tx),
-						layout1Transmitter.max_length.eq(1),
+						transmitter.stream.connect(interface.tx),
+						transmitter.max_length.eq(1),
 					]
 					for idx, altMode in self.altModes.items():
 						with m.If(idx == setup.index[0:8]):
-							m.d.comb += layout1Transmitter.data[0].eq(altMode)
+							m.d.comb += transmitter.data[0].eq(altMode)
 
 					# ... then trigger it when requested if the lengths match ...
 					with m.If(self.interface.data_requested):
 						with m.If(setup.length == 1):
-							m.d.comb += layout1Transmitter.start.eq(1)
+							m.d.comb += transmitter.start.eq(1)
 						with m.Else():
 							m.d.comb += interface.handshakes_out.stall.eq(1)
 							m.next = 'IDLE'
@@ -107,14 +136,14 @@ class AudioRequestHandler(USBRequestHandler):
 						setting, length = self.settingForCurrent(m, setup)
 						# Hook up the transmitter ...
 						m.d.comb += [
-							layout1Transmitter.stream.connect(interface.tx),
-							Cat(layout1Transmitter.data).eq(setting),
-							layout1Transmitter.max_length.eq(length),
+							transmitter.stream.connect(interface.tx),
+							Cat(transmitter.data).eq(setting),
+							transmitter.max_length.eq(length),
 						]
 						# ... then trigger it when requested if the lengths match ...
 						with m.If(self.interface.data_requested):
 							with m.If(setup.length == length):
-								m.d.comb += layout1Transmitter.start.eq(1)
+								m.d.comb += transmitter.start.eq(1)
 							with m.Else():
 								m.d.comb += interface.handshakes_out.stall.eq(1)
 								m.next = 'IDLE'
@@ -129,20 +158,20 @@ class AudioRequestHandler(USBRequestHandler):
 						length = self.lengthForCurrent(m, setup)
 						# Hook up the receiver ...
 						m.d.comb += [
-							interface.rx.connect(layout1Receiver.stream),
-							layout1Receiver.maxLength.eq(length),
+							interface.rx.connect(receiver.stream),
+							receiver.maxLength.eq(length),
 						]
 						# ... trigger the receiver if it isn't yet triggered ...
 						with m.If(~rxTriggered):
 							with m.If(setup.length == length):
-								m.d.comb += layout1Receiver.start.eq(1)
+								m.d.comb += receiver.start.eq(1)
 								m.d.usb += rxTriggered.eq(1)
 							with m.Else():
 								m.d.comb += interface.handshakes_out.stall.eq(1)
 								m.next = 'IDLE'
 						# ... then when the receiver finishes, write the resulting data to the setting
-						with m.Elif(layout1Receiver.done):
-							setting = Cat(layout1Receiver.data)
+						with m.Elif(receiver.done):
+							setting = Cat(receiver.data)
 							self.settingFromCurrent(m, setup, setting)
 
 						# If the current out packet is complete and the host is waiting for an ACK, make it
@@ -178,25 +207,40 @@ class AudioRequestHandler(USBRequestHandler):
 		m.d.comb += length.eq(0)
 
 		# If the request is for the power domain, return the length of the power state setting
-		with m.If((setup.index[0:8] == 0) & (setup.index[8:16] == 10) & (setup.value[0:8] == 0) &
-			(setup.value[8:16] == AudioControlInterfaceControlSelectors.AC_POWER_DOMAIN_CONTROL)):
+		with m.If(self._powerSelect):
 			m.d.comb += length.eq(1)
+		# If the request is for the functional unit mute controls, return the length of a mute setting
+		with m.Elif(self._muteSelect):
+			m.d.comb += length.eq(1)
+		# If the request is for the functional unit volume controls, return the length of a volume setting
+		with m.Elif(self._volumeSelect):
+			m.d.comb += length.eq(2)
 
 		return length
 
 	def settingForCurrent(self, m : Module, setup : SetupPacket):
-		setting = Signal(8)
+		setting = Signal(16)
 		m.d.comb += setting.eq(0)
 
 		# If the request is for the power domain, return the power state setting
-		with m.If((setup.index[0:8] == 0) & (setup.index[8:16] == 10) & (setup.value[0:8] == 0) &
-			(setup.value[8:16] == AudioControlInterfaceControlSelectors.AC_POWER_DOMAIN_CONTROL)):
-			m.d.comb += setting.eq(self.powerState)
+		with m.If(self._powerSelect):
+			m.d.comb += setting[0:8].eq(self.powerState)
+		# If the request is for the functional unit mute controls, return the length of a mute setting
+		with m.Elif(self._muteSelect):
+			m.d.comb += setting[0].eq(self.muteStates[setup.value[0:8]])
+		# If the request is for the functional unit volume controls, return the length of a volume setting
+		with m.Elif(self._volumeSelect):
+			m.d.comb += setting.eq(self.volumeStates[setup.value[0:8]])
 
 		return setting, self.lengthForCurrent(m, setup)
 
 	def settingFromCurrent(self, m : Module, setup : SetupPacket, setting : Signal):
 		# If the request is for the power domain, return the power state setting
-		with m.If((setup.index[0:8] == 0) & (setup.index[8:16] == 10) & (setup.value[0:8] == 0) &
-			(setup.value[8:16] == AudioControlInterfaceControlSelectors.AC_POWER_DOMAIN_CONTROL)):
-			m.d.usb += self.powerState.eq(setting)
+		with m.If(self._powerSelect):
+			m.d.usb += self.powerState.eq(setting[0:8])
+		# If the request is for the functional unit mute controls, return the length of a mute setting
+		with m.Elif(self._muteSelect):
+			m.d.usb += self.muteStates[setup.value[0:8]].eq(setting[0])
+		# If the request is for the functional unit volume controls, return the length of a volume setting
+		with m.Elif(self._volumeSelect):
+			m.d.usb += self.volumeStates[setup.value[0:8]].eq(setting)
