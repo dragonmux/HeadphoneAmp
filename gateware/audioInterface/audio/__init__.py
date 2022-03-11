facbeac6baa77fc6642c2c250d00ce55f6a2a957
dragonmux <git@dragonmux.network>
2022-03-11 17:16:17 -0500

    gateware: audio: begun implementing the audio domain logic

---
+++ gateware/audioInterface/audio/__init__.py
@@ -0,0 +1,30 @@
+from amaranth import Elaboratable, Module, Cat
+from amaranth.lib.fifo import AsyncFIFO
+
+from .i2s import I2S
+
+__all__ = (
+	'AudioStream'
+)
+
+class AudioStream(Elaboratable):
+	def __init__(self):
+		self.audioFIFO = AsyncFIFO(width = 48, depth = 256, r_domain = 'sync', w_domain = 'usb')
+
+	def elaborate(self, platform):
+		m = Module()
+		# m.d.sync is the audio domain.
+		m.submodules.audioFIFO = self.audioFIFO
+		m.submodules.i2s = i2s = I2S()
+
+		fifo = self.audioFIFO
+		m.d.comb += fifo.r_en.eq(0)
+
+		with m.If(i2s.needSample):
+			with m.If(fifo.r_rdy):
+				m.d.sync += Cat(i2s.sample).eq(fifo.r_data)
+				m.d.comb += fifo.r_en.eq(1)
+			with m.Else():
+				m.d.sync += Cat(i2s.sample).eq(0)
+
+		return m
