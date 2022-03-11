80538b04e7ed1d2081395d661c71fbd4eccdc946
dragonmux <git@dragonmux.network>
2022-03-11 17:48:40 -0500

    gateware: interface: Added use of the audio module

--- gateware/audioInterface/interface.py
+++ gateware/audioInterface/interface.py
@@ -1,5 +1,6 @@
 from amaranth import Elaboratable, Module, ClockDomain, ResetSignal
 from .usb import USBInterface
+from .audio import AudioStream
 
 __all__ = (
 	'AudioInterface',
@@ -9,7 +10,8 @@ class AudioInterface(Elaboratable):
 	def elaborate(self, platform):
 		m = Module()
 		m.domains += ClockDomain('usb')
-		m.submodules.usb = USBInterface(resource = ('ulpi', 0))
+		m.submodules.usb = usb = USBInterface(resource = ('ulpi', 0))
+		m.submodules.audio = AudioStream(usb)
 
 		m.d.comb += ResetSignal('usb').eq(0)
 		return m
