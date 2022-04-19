f772ac59eccd7a16d22dc03000c02bbd3b3846c4
dragonmux <git@dragonmux.network>
2022-04-18 20:11:23 -0400

    Re-timed the DFURequestHandler so timing passes

--- gateware/audioInterface/usb/control/dfu.py
+++ gateware/audioInterface/usb/control/dfu.py
@@ -36,66 +36,65 @@ class DFURequestHandler(USBRequestHandler):
 		triggerReboot = Signal()
 		warmbootSelect = Signal(2)
 
-		with m.If(self.handlerCondition(setup)):
-			with m.FSM(domain = 'usb', name = 'dfu'):
-				with m.State('IDLE'):
-					with m.If(setup.received):
-						with m.Switch(setup.request):
-							with m.Case(DFURequests.DETACH):
-								m.next = 'HANDLE_DETACH'
-							with m.Case(DFURequests.GET_STATUS):
-								m.next = 'HANDLE_GET_STATUS'
-							with m.Case(DFURequests.GET_STATE):
-								m.next = 'HANDLE_GET_STATE'
-							with m.Default():
-								m.next = 'UNHANDLED'
+		with m.FSM(domain = 'usb', name = 'dfu'):
+			with m.State('IDLE'):
+				with m.If(setup.received & self.handlerCondition(setup)):
+					with m.Switch(setup.request):
+						with m.Case(DFURequests.DETACH):
+							m.next = 'HANDLE_DETACH'
+						with m.Case(DFURequests.GET_STATUS):
+							m.next = 'HANDLE_GET_STATUS'
+						with m.Case(DFURequests.GET_STATE):
+							m.next = 'HANDLE_GET_STATE'
+						with m.Default():
+							m.next = 'UNHANDLED'
 
-				with m.State('HANDLE_DETACH'):
-					m.d.comb += triggerReboot.eq(1)
+			with m.State('HANDLE_DETACH'):
+				m.d.comb += triggerReboot.eq(1)
 
-				with m.State('HANDLE_GET_STATUS'):
-					m.d.comb += [
-						transmitter.stream.connect(interface.tx),
-						transmitter.max_length.eq(6),
-						transmitter.data[0].eq(DFUStatus.ok),
-						Cat(transmitter.data[1:4]).eq(0),
-						transmitter.data[4].eq(DFUState.appIdle),
-						transmitter.data[5].eq(0),
-					]
+			with m.State('HANDLE_GET_STATUS'):
+				m.d.comb += [
+					transmitter.stream.connect(interface.tx),
+					transmitter.max_length.eq(6),
+					transmitter.data[0].eq(DFUStatus.ok),
+					Cat(transmitter.data[1:4]).eq(0),
+					transmitter.data[4].eq(DFUState.appIdle),
+					transmitter.data[5].eq(0),
+				]
 
-					with m.If(interface.data_requested):
-						with m.If(setup.length == 6):
-							m.d.comb += transmitter.start.eq(1)
-						with m.Else():
-							m.d.comb += interface.handshakes_out.stall.eq(1)
-							m.next = 'IDLE'
-
-					with m.If(interface.status_requested):
-						m.d.comb += interface.handshakes_out.ack.eq(1)
-						m.next = 'IDLE'
-
-				with m.State('HANDLE_GET_STATE'):
-					m.d.comb += [
-						transmitter.stream.connect(interface.tx),
-						transmitter.max_length.eq(1),
-						transmitter.data[0].eq(DFUState.appIdle),
-					]
-
-					with m.If(interface.data_requested):
-						with m.If(setup.length == 1):
-							m.d.comb += transmitter.start.eq(1)
-						with m.Else():
-							m.d.comb += interface.handshakes_out.stall.eq(1)
-							m.next = 'IDLE'
-
-					with m.If(interface.status_requested):
-						m.d.comb += interface.handshakes_out.ack.eq(1)
-						m.next = 'IDLE'
-
-				with m.State('UNHANDLED'):
-					with m.If(interface.data_requested | interface.status_requested):
+				with m.If(interface.data_requested):
+					with m.If(setup.length == 6):
+						m.d.comb += transmitter.start.eq(1)
+					with m.Else():
 						m.d.comb += interface.handshakes_out.stall.eq(1)
 						m.next = 'IDLE'
+
+				with m.If(interface.status_requested):
+					m.d.comb += interface.handshakes_out.ack.eq(1)
+					m.next = 'IDLE'
+
+			with m.State('HANDLE_GET_STATE'):
+				m.d.comb += [
+					transmitter.stream.connect(interface.tx),
+					transmitter.max_length.eq(1),
+					transmitter.data[0].eq(DFUState.appIdle),
+				]
+
+				with m.If(interface.data_requested):
+					with m.If(setup.length == 1):
+						m.d.comb += transmitter.start.eq(1)
+					with m.Else():
+						m.d.comb += interface.handshakes_out.stall.eq(1)
+						m.next = 'IDLE'
+
+				with m.If(interface.status_requested):
+					m.d.comb += interface.handshakes_out.ack.eq(1)
+					m.next = 'IDLE'
+
+			with m.State('UNHANDLED'):
+				with m.If(interface.data_requested | interface.status_requested):
+					m.d.comb += interface.handshakes_out.stall.eq(1)
+					m.next = 'IDLE'
 
 		m.submodules += Instance(
 			'SB_WARMBOOT',
