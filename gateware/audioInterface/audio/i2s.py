2456b0d2f7c21fadc6aad08cb0b976a664c846ad
dragonmux <git@dragonmux.network>
2022-10-01 13:26:19 +0100

    gateware: audio.i2s: Begun documenting the state machine logic

--- gateware/audioInterface/audio/i2s.py
+++ gateware/audioInterface/audio/i2s.py
@@ -69,15 +69,21 @@ class I2S(Elaboratable):
 					m.d.sync += clkCounter.eq(clkCounter + 1)
 
 			with m.State('RUN'):
+				# Each time the system clock to data clock counter fills to the set value,
+				# Do one half of the audio data clock cycle.
 				with m.If(clkCounter == self.clkDivider):
 					m.d.sync += [
 						clkCounter.eq(0),
 						audioClk.eq(~audioClk),
 					]
 
+					# If the audio clock was high put the next sample bit out.
 					with m.If(audioClk):
+						# If we've put all the sample bits out for this channel then reset the sample bit
+						# counter, and switch to the other channel.
 						with m.If(sampleBit == self.sampleBits):
 							m.d.sync += sampleBit.eq(0)
+						# Else we need to put out the next sample bit and count up.
 						with m.Else():
 							with m.If(sampleBit == lastBit):
 								m.d.sync += channelNext.eq(~channelNext)
@@ -85,6 +91,7 @@ class I2S(Elaboratable):
 
 						m.d.sync += channelCurrent.eq(channelNext)
 
+				# Otherwise count up on the system clock domain
 				with m.Else():
 					m.d.sync += clkCounter.eq(clkCounter + 1)
 
