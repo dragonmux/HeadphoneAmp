30cbdc18ab0fdbc95217f85f0a1a7607f9e67adc
dragonmux <git@dragonmux.network>
2022-10-01 13:22:47 +0100

    gateware: audio.i2s: Corrected the timings for the generation of the I²S rnl signal and re-timed the things that hang off it

--- gateware/audioInterface/audio/i2s.py
+++ gateware/audioInterface/audio/i2s.py
@@ -1,8 +1,13 @@
+from enum import IntEnum
 from amaranth import Elaboratable, Module, Signal, Array, Cat
 
 __all__ = (
 	'I2S',
 )
+
+class Channel(IntEnum):
+	left = 0
+	right = 1
 
 class I2S(Elaboratable):
 	def __init__(self):
@@ -19,21 +24,27 @@ class I2S(Elaboratable):
 		clkCounter = Signal.like(self.clkDivider)
 		audioClk = Signal(reset = 1)
 		sampleBit = Signal(range(24))
-		# 0 = Left, 1 = Right
-		channel = Signal(reset = 1)
+		lastBit = Signal(range(24))
+		channelCurrent = Signal(Channel, reset = Channel.left)
+		channelNext = Signal(Channel, reset = Channel.right)
 		sample = Array(Signal() for i in range(24))
 
 		sampleLatch = Signal()
-		m.d.sync += sampleLatch.eq(channel)
-		m.d.comb += self.needSample.eq((~channel) & sampleLatch)
+		m.d.sync += sampleLatch.eq(channelCurrent)
+		m.d.comb += self.needSample.eq((~channelCurrent) & sampleLatch)
 
 		with m.FSM():
 			with m.State('IDLE'):
-				m.d.sync += sampleLatch.eq(0)
+				m.d.sync += [
+					sampleLatch.eq(0),
+					channelCurrent.eq(Channel.left),
+					channelNext.eq(Channel.right),
+				]
 				with m.If((self.sampleBits != 0) & (self.clkDivider != 0)):
 					m.d.sync += [
 						clkCounter.eq(self.clkDivider),
 						sampleBit.eq(self.sampleBits),
+						lastBit.eq(self.sampleBits - 1),
 					]
 					m.next = 'RUN'
 			with m.State('RUN'):
@@ -45,21 +56,21 @@ class I2S(Elaboratable):
 
 					with m.If(audioClk):
 						with m.If(sampleBit == self.sampleBits):
-							m.d.sync += [
-								channel.eq(~channel),
-								sampleBit.eq(0),
-							]
+							m.d.sync += sampleBit.eq(0)
 						with m.Else():
+							with m.If(sampleBit == lastBit):
+								m.d.sync += channelNext.eq(~channelNext)
 							m.d.sync += sampleBit.eq(sampleBit + 1)
+
+						m.d.sync += channelCurrent.eq(channelNext)
 
 				with m.Else():
 					m.d.sync += clkCounter.eq(clkCounter + 1)
 
 		m.d.comb += [
-			Cat(sample).eq(self.sample[channel]),
-			bus.clk.eq(audioClk),
-			bus.rnl.eq(channel),
-			# TODO: Put the bits out MSB to LSB.
-			bus.data.eq(sample[sampleBit]),
+			Cat(sample).eq(self.sample[channelCurrent]),
+			bus.clk.o.eq(audioClk),
+			bus.rnl.o.eq(channelNext),
+			bus.data.o.eq(sample[self.sampleBits - sampleBit]),
 		]
 		return m
