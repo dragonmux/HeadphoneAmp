7fc91a4b41e99b7e27639fc5a1504b825fff860c
dragonmux <git@dragonmux.network>
2023-10-23 10:16:13 +0100

    gateware: audio.spdif: Implemented the first part of 'Z' sync sequence handling

--- gateware/audioInterface/audio/spdif/timing.py
+++ gateware/audioInterface/audio/spdif/timing.py
@@ -32,8 +32,97 @@ class Timing(Elaboratable):
 	def __init__(self):
 		self.spdifIn = Signal()
 
+		self.reset = Signal(reset = 1)
+		self.sync = Signal(reset = 1)
+		self.begin = Signal()
+		self.channel = Signal()
+		self.bitClock = Signal()
+
 	def elaborate(self, platform : Platform) -> Module:
 		m = Module()
-		#
 
+		dataInPrev = Signal()
+		dataInCurr = Signal()
+
+		# The ranges 570 and 190 have been chosen as upper limits based on the minimum sampling frequency
+		# being 32kHz, giving rise to `60MHz / 32kHz` = 187.5. The actual bit timing for 32kHz audio would
+		# have to be higher than this to accomodate protocol overheads (there are 32 bit periods per sample
+		# required), so this should be more than enough for any valid data stream.
+		longTimer = Signal(range(570))
+		shortTimer = Signal(range(190))
+		bitTime = Signal(range(190))
+		longTime = Signal(range(570))
+
+		with m.FSM(domain = 'usb') as syncFSM:
+			m.d.comb += [
+				self.reset.eq(syncFSM.ongoing('IDLE')),
+			]
+
+			# In this state we are only looking for a transition on spdifIn (look for the channel to become active).
+			with m.State('IDLE'):
+				m.d.usb += longTimer.eq(0)
+				with m.If(dataInPrev != dataInCurr):
+					m.next = 'SYNC-Z-BEGIN'
+
+			# Start looking for a Z preamble
+			with m.State('SYNC-Z-BEGIN'):
+				m.d.usb += longTimer.eq(longTimer + 1)
+				# If we've not had a transition in too long and the timer is about to expire, reset
+				# back to the IDLE state as the link should now be treated as 'idle'
+				with m.If(longTimer == 569):
+					m.next = 'IDLE'
+				with m.Elif(dataInPrev != dataInCurr):
+					m.d.usb += shortTimer.eq(0)
+					m.next = 'SYNC-Z-SHORT1'
+
+			# Look for the first short bit period of the sync sequence.
+			with m.State('SYNC-Z-SHORT1'):
+				# If we've not had a transition in too long and we've exceeded the long bit time, we
+				# might have captured too short of a long bit time and instead have grabbed a BMC bit
+				# so copy the timer value back to the long timer and switch back
+				with m.If(shortTimer == longTimer):
+					m.d.usb += longTimer.eq(shortTimer + 1)
+					m.next = 'SYNC-Z-BEGIN'
+				# Otherwise if the timer is about to expire, abort back to idle
+				with m.Elif(shortTimer == 189):
+					m.next = 'IDLE'
+				# Hopefully we just got a short bit time, so capture it, reset the timer and check for the second
+				with m.Elif(dataInPrev != dataInCurr):
+					m.d.usb += [
+						bitTime.eq(shortTimer),
+						shortTimer.eq(0),
+					]
+					m.next = 'SYNC-Z-SHORT2'
+				with m.Else():
+					m.d.usb += shortTimer.eq(shortTimer + 1)
+
+			with m.State('SYNC-Z-SHORT2'):
+				# If we exceed the previous bit time by more than a couple of counts, more than likely
+				# this was not a Z sync sequence. Abort back to idle.
+				with m.If((shortTimer == (bitTime + 3)) & dataInPrev == dataInCurr):
+					m.next = 'IDLE'
+				# If the timer is about to expire, abort back to idle.
+				with m.Elif(shortTimer == 189):
+					m.next = 'IDLE'
+				# We got something that should be another short bit, so let's check the timing is with
+				# a couple of counts of the expected bit time
+				with m.Elif(dataInPrev != dataInCurr):
+					with m.If(shortTimer > (bitTime - 3)):
+						m.next = 'SYNC-Z-FINAL'
+					# If it falls outside range, chances are we got something else like a BMC bit or something
+					# not right, so go back to trying to capture a Z sync sequence
+					with m.Else():
+						m.d.usb += longTimer.eq(0)
+						m.next = 'SYNC-Z-BEGIN'
+				with m.Else():
+					m.d.usb += shortTimer.eq(shortTimer + 1)
+
+			with m.State('SYNC-Z-FINAL'):
+				pass
+
+		# Synchronise the input S/PDIF signal and time delay it to allow us to detect edges
+		m.d.usb += [
+			dataInCurr.eq(self.spdifIn),
+			dataInPrev.eq(dataInCurr),
+		]
 		return m
